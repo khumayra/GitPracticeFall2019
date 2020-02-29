a780c4762ed6ae4c1f61995cdcb1adde4bae1805
khumayra <khumayra@gmail.com>
2020-02-29 16:05:34 -0500

    Added new loop practice

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -1,4 +1,11 @@
 package day1;
 
 public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 2; i < 8; i++) {
+            System.out.println("hello");
+            System.out.println("tralala");
+            System.out.println("git is fun :)");
+        }
+    }
 }
